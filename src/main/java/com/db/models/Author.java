579c98754af3c2d0f09203e8f5a514392e2097e1
EranTimothy-dev <eranperera.dev@gmail.com>
2025-05-20 00:15:25 +0530

    added Author model

--- src/main/java/com/db/models/Author.java
+++ src/main/java/com/db/models/Author.java
@@ -1,2 +1,56 @@
-package com.db.models;public class Author {
+package com.db.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "Author_Tbl", schema = "public")
+public class Author {
+
+    @Id
+    @GeneratedValue
+//            (
+//            strategy = GenerationType.TABLE,
+//            generator = "author_id_gen"
+//    )
+//    @TableGenerator(
+//            name = "author_id_gen",
+//            table = "id_generator",
+//            pkColumnName = "id_name",
+//            valueColumnName = "id_value",
+//            allocationSize = 1
+//    )
+//    @SequenceGenerator(
+//            name = "author_sequence",
+//            sequenceName = "author_sequence",
+//            allocationSize = 1
+//    )
+    private Integer id;
+    @Column(
+            name = "f_name",
+            length = 35
+    )
+    private String firstName;
+    private String lastName;
+    @Column(
+            unique = true,
+            nullable = false
+    )
+    private String email;
+    private int age;
+    @Column(
+            updatable = false,
+            nullable = false
+    )
+    private LocalDateTime createdAt;
+    @Column(
+            insertable = false
+    )
+    private LocalDateTime lastModified;
+
 }
