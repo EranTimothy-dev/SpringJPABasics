cc3bb87d1717dcfb916773103267ae9757bd47cc
EranTimothy-dev <eranperera.dev@gmail.com>
2025-05-20 19:01:53 +0530

    using embedded entities for composite keys

---
+++ src/main/java/com/db/models/embedded/Order.java
@@ -0,0 +1,21 @@
+package com.db.models.embedded;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @EmbeddedId
+    private OrderId id;
+    private String orderInfo;
+    private String anotherField;
+}
